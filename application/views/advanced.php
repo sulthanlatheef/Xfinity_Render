fc2c2fcf5424eb18f86724f494c679f266a2f5a9
sulthanlatheef <sulthanlatheef0@gmail.com>
2025-03-01 23:57:04 +0530

    corrected session handling

--- application/views/advanced.php
+++ application/views/advanced.php
@@ -5,7 +5,7 @@
     <title>Advanced Page</title>
 </head>
 <body>
-    <h1>Welcome to the Advanced Page, <?php echo $this->session->userdata('username'); ?>!</h1>
+    <h1>Welcome to the Advanced Page, <?php echo $this->session->userdata('name'); ?>!</h1>
     <p>This page is accessible only to logged-in users.</p>
     <a href="<?php echo site_url('home/logout'); ?>">Logout</a>
 </body>
