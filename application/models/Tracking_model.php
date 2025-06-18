0fd5acec1e567744e6548a991f255995b663b9ed
sulthanlatheef <sulthanlatheef0@gmail.com>
2025-06-18 13:12:03 +0530

    31

--- application/models/Tracking_model.php
+++ application/models/Tracking_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class tracking_model extends CI_Model {
+class Tracking_model extends CI_Model {
 
     public function __construct()
     {
