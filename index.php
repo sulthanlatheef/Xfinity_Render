67c56c42f7d01d7c0f099bf7c96700d455f8901d
sulthanlatheef <sulthanlatheef0@gmail.com>
2025-12-21 00:58:12 +0530

    569

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->safeLoad();
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
+
+
 
 
 /**
